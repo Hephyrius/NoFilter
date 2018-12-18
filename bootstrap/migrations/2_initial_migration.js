319dca65bb7b0c1f247c1206a84616a1b9c1c121
Harnick Khera <Hephyrius@gmail.com>
2018-12-18 19:45:03 +0000

    Update 2_initial_migration.js

--- bootstrap/migrations/2_initial_migration.js
+++ bootstrap/migrations/2_initial_migration.js
@@ -1,7 +1,8 @@
 var DiceGame = artifacts.require("./DiceGame.sol");
+var treddit = artifacts.require ("./Treddit.sol");
 
 module.exports = function(deployer) {
 
   deployer.deploy(DiceGame);
-
+  deployer.deploy(treddit);
 };
