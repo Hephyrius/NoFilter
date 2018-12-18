a72d85a8363e83a650372b52713256aadc0b4174
Harnick Khera <Hephyrius@gmail.com>
2018-12-18 21:23:08 +0000

    created an environment to deploy from

---
+++ coin/contracts/Treddit.sol
@@ -0,0 +1,150 @@
+pragma solidity >=0.4.2;
+
+contract Treddit {
+    
+    //post related variables
+    uint postNumber; // keeps track of posts so that every one is unique
+    mapping (uint => address) postOwners; //Keeps track of who owns a specific post
+    mapping (uint => uint) votes; // keeps track of a posts vote counter
+    mapping (address => mapping(uint => uint)) voters; //keeps track of a users voting history, helps to prevent vote spamming from a single account
+    
+    //comment related variables
+    mapping (uint => uint) commentCounter; // keeps track of posts so that every one is unique
+    mapping (uint => mapping(uint => address)) commentOwner; //Keeps track of who owns a specific post
+    mapping (uint => mapping(uint => uint)) commentVotes; // keeps track of a posts vote counter
+    mapping (address => mapping(uint => mapping(uint => uint))) commentVoters; //keeps track of a users voting history, helps to prevent vote spamming from a single account
+    
+    constructor () public {
+        postNumber = 0;
+    }
+    
+    //Invoked when the post has been created, so that the dApp can access the content
+    event PostContent (
+        address indexed author,
+        uint indexed id,
+        bytes indexed tags,
+        uint postTimestamp, //used to generate private post unique key
+        bytes title,
+        bytes text
+    );
+    
+    //create a new post, done by storing data in the logs. data can be in any form as long as it is byte data. This means data can be compressed
+    function CreatePost(bytes title, bytes text, bytes tags) public {
+        
+        //check that the data is correct
+        require(title.length > 0, "Invalid title");
+        require(text.length > 0, "Invalid text");
+        require(tags.length > 0, "Invalid tags");
+        
+        uint postId = postNumber;
+        //init the mapping and set the owner as the sender
+        postOwners[postId] = msg.sender;
+        
+        //init votes so that the owner has upvoted by default.
+        votes[postId] = 1;
+        voters[msg.sender][postId] = 1;
+        
+        //posts are stored in logs, it allows to reduce post invocation cost
+        emit PostContent(msg.sender, postId, tags, now, title, text);
+        
+        postNumber = postId + 1;
+    }
+    
+    //get the owner of a post
+    function getOwner(uint postId) public view returns (address) {
+        return postOwners[postId];
+    }
+
+    //stores a log comment on the blockchain when commented
+    event CommentCreated (
+        address indexed commenter,
+        uint indexed postId,
+        bytes comment,
+        uint parentComment
+    );
+    
+    function PostComment(uint postId, bytes text, uint parentComment) public {
+        require(postId >= 0, "comment is not for a valid post");
+        require(postId < postNumber, "comment is for a non existent post");
+        require(text.length > 0, "comment comment is empty");
+        emit CommentCreated(msg.sender, postId, text, parentComment);
+        
+        //update comment related variables
+        uint commentId = commentCounter[postId];
+        commentOwner[postId][commentId] = msg.sender;
+        
+        //deal with upvote data
+        commentVotes[postId][commentId] = 1;
+        commentVoters[msg.sender][postId][commentId] = 1;
+        
+        //increment counter when done
+        commentCounter[postId] += 1;
+    }
+    
+    //upvote a post 
+    function UpvotePost(uint postId) public {
+        require(postId >= 0, "votes is not for a valid post");
+        require(postId < postNumber, "votes is for a non existent post");
+        
+        require(voters[msg.sender][postId] != 1, "Upvote already done");
+        voters[msg.sender][postId] = 1;
+        votes[postId] += 1;
+    }
+    
+    //downvote a post 
+    function DownvotePost(uint postId) public {
+        require(postId >= 0, "votes is not for a valid post");
+        require(postId < postNumber, "votes is for a non existent post");
+        
+        require(voters[msg.sender][postId] != 2, "downvote already done");
+        voters[msg.sender][postId] = 2;
+        votes[postId] -= 1;
+    }
+    
+    //get the vote data
+    function getVotes(uint postId) public view returns (uint) {
+        return votes[postId];
+    }
+    
+    //get the voter data
+    function getVoterData(uint postId, address voter) public view returns (uint) {
+        return voters[voter][postId];
+    }
+    
+    //upvote a post 
+    function UpvoteComment(uint postId, uint commendId) public {
+        require(postId >= 0, "votes is not for a valid post");
+        require(postId < postNumber, "votes is for a non existent post");
+        
+        require(commendId >= 0, "comment id is not valid");
+        require(commendId <= commentCounter[postId], "comment id is non existent");
+        require(commentVoters[msg.sender][postId][commendId] != 1, "Upvote already done");
+        
+        commentVoters[msg.sender][postId][commendId] = 1;
+        commentVotes[postId][commendId] += 1;
+    }
+    
+    //downvote a post 
+    function DownvoteComment(uint postId, uint commendId) public {
+        require(postId >= 0, "votes is not for a valid post");
+        require(postId < postNumber, "votes is for a non existent post");
+        
+        require(commendId >= 0, "comment id is not valid");
+        require(commendId <= commentCounter[postId], "comment id is non existent");
+        require(commentVoters[msg.sender][postId][commendId] != 2, "Upvote already done");
+        
+        commentVoters[msg.sender][postId][commendId] = 2;
+        commentVotes[postId][commendId] -= 1;
+    }
+    
+    //get the comment vote data
+    function getCommentVotes(uint postId, uint commendId) public view returns (uint) {
+        return commentVotes[postId][commendId];
+    }
+    
+    //get the comment voter data
+    function getCommentVoterData(uint postId, uint commendId, address commenter) public view returns (uint) {
+        return commentVoters[commenter][postId][commendId];
+    } 
+
+}
---
+++ coin/migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var treddit = artifacts.require ("./Treddit.sol");
+
+module.exports = function(deployer) {
+	deployer.deploy(treddit);
+};
