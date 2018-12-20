1c532f957896305fe13762bb024767171fa0d5c7
Harnick Khera <Hephyrius@gmail.com>
2018-12-20 19:49:45 +0000

    updated post page layout

--- React/treddit/src/components/PostPage.js
+++ React/treddit/src/components/PostPage.js
@@ -2,11 +2,39 @@ import React, { Component } from 'react';
 class PostPage extends Component {
 
   render() {
-    let postItems;
-    let postid = this.params.postid;
-    console.log(postid);
+    let postid;
+    let post;
+    if(this.props.postid) {
+      console.log(this.props.postid)
+      let posts = JSON.parse(localStorage.getItem("Posts"));
+      for(var i=0; i<posts.length; i++){
+        if(posts[i]['postid'] === this.props.postid){
+          post = posts[i];
+          break;
+        }
+      } 
+
+    }
     return (
       <div className="PostPage">
+        <div class="container">
+          <div class="row">
+
+              <h1 class="mt-4">{post['title']}</h1>
+
+              <p></p>
+
+              <p class="lead">{post['content']} </p>
+
+              <p></p>
+
+              <p>Posted on {post['timestamp']}  </p>
+
+              <p></p>
+
+              <p> - by {post['author']}</p>
+          </div>
+        </div>
       </div>
     );
   }
