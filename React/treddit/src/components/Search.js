d337ab86a3b33e07059ba95f9138ce8720b21576
Harnick Khera <Hephyrius@gmail.com>
2018-12-28 23:09:18 +0000

    Formatting Fix

--- React/treddit/src/components/Search.js
+++ React/treddit/src/components/Search.js
@@ -23,20 +23,20 @@ class Search extends Component {
       render() {
         return (
             <div id="container" class="container">
-                    <div class="row text-black">
-                        <div class="col-sm-10 offset-sm-1 text-center">
-                            <h2 class="display-3">Search Posts</h2>
-                                <div class="info-form">
-                                    <form onSubmit={this.handleSubmit}>  
-                                        <div class="form-group">
-                                            <input type="text" value={this.state.search} onChange={this.handleSearchChange} />
-                                        </div>
-                                        <input type="submit" class="btn btn-outline-dark" value="Search All Posts" />
-                                    </form>
-                                </div>
-                            </div>
+                <div class="row text-black">
+                    <div class="col-sm-10 offset-sm-1 text-center">
+                        <h2 class="display-3">Search Posts</h2>
+                            <div class="info-form">
+                                <form onSubmit={this.handleSubmit}>  
+                                    <div class="form-group">
+                                        <input type="text" value={this.state.search} onChange={this.handleSearchChange} />
+                                    </div>
+                                <input type="submit" class="btn btn-outline-dark" value="Search All Posts" />
+                            </form>
                         </div>
+                    </div>
                 </div>
+            </div>
         );
       }
     }
