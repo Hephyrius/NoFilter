454809121b442e085323336b24886ff6a6e0aa65
Harnick Khera <Hephyrius@gmail.com>
2019-01-09 21:14:36 +0000

    Added event server notice

--- React/src/components/DummyItem.js
+++ React/src/components/DummyItem.js
@@ -18,7 +18,7 @@ class DummyItem extends Component {
       <div className="DummyItem">
         <div class="container">
             <div class="row">
-              There are no {type}'s to display. Try refreshing the page to load events from the Tron Virtual Machine.
+              There are no {type}'s to display. Try refreshing the page to load events from the Tron Virtual Machine. If no events are shown, try pointing your tronlink to another event server as api.trongrid.io will only show events from the last ~6 hours.
             </div>
           </div>
         </div>
