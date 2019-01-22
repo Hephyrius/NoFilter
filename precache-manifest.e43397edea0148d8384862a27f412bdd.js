9dc657395a3aac3ef5d355de0376e77d642fcb93
Harnick Khera <Hephyrius@gmail.com>
2019-01-22 00:25:16 +0000

    Github Pages Setup

---
+++ precache-manifest.e43397edea0148d8384862a27f412bdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "05d53b68c33b0cf5b70c",
+    "url": "/static/js/main.05d53b68.chunk.js"
+  },
+  {
+    "revision": "b8712de1c6c31bfc0012",
+    "url": "/static/js/1.b8712de1.chunk.js"
+  },
+  {
+    "revision": "05d53b68c33b0cf5b70c",
+    "url": "/static/css/main.f601a868.chunk.css"
+  },
+  {
+    "revision": "b8712de1c6c31bfc0012",
+    "url": "/static/css/1.640f9e6a.chunk.css"
+  },
+  {
+    "revision": "e81cf4b36c3c53e08127436f350e169a",
+    "url": "/index.html"
+  }
+];
